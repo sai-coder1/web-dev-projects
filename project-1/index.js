27a768783c6a0636cffcc02904be8fbc2ad92176
sai <saibhumireddy@email.com>
2026-07-03 11:33:29 +0530

    Added color Flipper Project

---
+++ project-1/index.js
@@ -0,0 +1,15 @@
+const overlay = document.getElementById("overlay");
+
+function setColor(name) {
+    overlay.style.backgroundColor = name;
+}
+
+function randomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    const color = `rgba(${red},${green},${blue},0.7)`;
+
+    overlay.style.backgroundColor = color;
+}
